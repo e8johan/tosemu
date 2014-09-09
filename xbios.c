7f931721552172d6577ec30687ff7a0c3d01e67a
Johan Thelin <johan.thelin@pelagicore.com>
2014-09-09 06:53:50 +0200

    Added complete function table for XBIOS

--- xbios.c
+++ xbios.c
@@ -27,12 +27,125 @@
 #include "m68k.h"
 #include "utils.h"
 
+/* XBIOS functions */
+
 uint32_t XBIOS_Getrez(uint32_t sp)
 {
     /* Custom value, to ensure that HW-dependent code fails */
     return 8;
 }
 
+/* Table of non-implemented XBIOS functions */
+
+#define XBIOS_Bconmap NULL
+#define XBIOS_Bioskeys NULL
+#define XBIOS_Blitmode NULL
+#define XBIOS_Buffoper NULL
+#define XBIOS_Buffptr NULL
+#define XBIOS_Cursconf NULL
+#define XBIOS_Dbmsg NULL
+#define XBIOS_Devconnect NULL
+#define XBIOS_DMAread NULL
+#define XBIOS_DMAwrite NULL
+#define XBIOS_Dosound NULL
+#define XBIOS_Dsp_Available NULL
+#define XBIOS_Dsp_BlkBytes NULL
+#define XBIOS_Dsp_BlkHandShake NULL
+#define XBIOS_Dsp_BlkUnpacked NULL
+#define XBIOS_Dsp_BlkWords NULL
+#define XBIOS_Dsp_DoGlock NULL
+#define XBIOS_Dsp_ExecBoot NULL
+#define XBIOS_Dsp_ExecProg NULL
+#define XBIOS_Dsp_FlushSubroutines NULL
+#define XBIOS_Dsp_GetProgAbility NULL
+#define XBIOS_Dsp_GetWordSize NULL
+#define XBIOS_Dsp_Hf0 NULL
+#define XBIOS_Dsp_Hf1 NULL
+#define XBIOS_Dsp_Hf2 NULL
+#define XBIOS_Dsp_Hf3 NULL
+#define XBIOS_Dsp_HStat NULL
+#define XBIOS_Dsp_InqSubrAbility NULL
+#define XBIOS_Dsp_InStream NULL
+#define XBIOS_Dsp_IOStream NULL
+#define XBIOS_Dsp_LoadProg NULL
+#define XBIOS_Dsp_LoadSubroutine NULL
+#define XBIOS_Dsp_Lock NULL
+#define XBIOS_Dsp_LodToBunary NULL
+#define XBIOS_Dsp_MultBlocks NULL
+#define XBIOS_Dsp_OutStream NULL
+#define XBIOS_Dsp_RemoveInterrupts NULL
+#define XBIOS_Dsp_RequestUniqueAbility NULL
+#define XBIOS_Dsp_Reserve NULL
+#define XBIOS_Dsp_RunSubroutine NULL
+#define XBIOS_Dsp_SetVectors NULL
+#define XBIOS_Dsp_TriggerHC NULL
+#define XBIOS_Dsp_Unlock NULL
+#define XBIOS_Dsptristate NULL
+#define XBIOS_EgetPalette NULL
+#define XBIOS_EgetShift NULL
+#define XBIOS_EsetBank NULL
+#define XBIOS_EsetColor NULL
+#define XBIOS_EsetGray NULL
+#define XBIOS_EsetPalette NULL
+#define XBIOS_EsetShift NULL
+#define XBIOS_EsetSmear NULL
+#define XBIOS_Flopfmt NULL
+#define XBIOS_Floprate NULL
+#define XBIOS_Floprd NULL
+#define XBIOS_Flopver NULL
+#define XBIOS_Flopwr NULL
+#define XBIOS_Gettime NULL
+#define XBIOS_Glaccess NULL
+#define XBIOS_Gpio NULL
+#define XBIOS_Ikbdws NULL
+#define XBIOS_Initmous NULL
+#define XBIOS_Iorec NULL
+#define XBIOS_Jdisint NULL
+#define XBIOS_Jenabint NULL
+#define XBIOS_Kbdvbase NULL
+#define XBIOS_Kbrate NULL
+#define XBIOS_Keytbl NULL
+#define XBIOS_Locksnd NULL
+#define XBIOS_Logbase NULL
+#define XBIOS_Metainit NULL
+#define XBIOS_Mfpint NULL
+#define XBIOS_Midiws NULL
+#define XBIOS_NVMaccess NULL
+#define XBIOS_Offgibit NULL
+#define XBIOS_Ongibit NULL
+#define XBIOS_Physbse NULL
+#define XBIOS_Protobt NULL
+#define XBIOS_Prtblk NULL
+#define XBIOS_Puntaes NULL
+#define XBIOS_Random NULL
+#define XBIOS_Rsconf NULL
+#define XBIOS_Scrdmp NULL
+#define XBIOS_Setbuffer NULL
+#define XBIOS_Setcolor NULL
+#define XBIOS_Setinterrupt NULL
+#define XBIOS_Setmode NULL
+#define XBIOS_Setmontracks NULL
+#define XBIOS_Setpalette NULL
+#define XBIOS_Setprt NULL
+#define XBIOS_Setscreen NULL
+#define XBIOS_Settime NULL
+#define XBIOS_Settracks NULL
+#define XBIOS_Sndstatus NULL
+#define XBIOS_Soundcmd NULL
+#define XBIOS_Ssbrk NULL
+#define XBIOS_Supexec NULL
+#define XBIOS_Unlocksnd NULL
+#define XBIOS_VgetMonitor NULL
+#define XBIOS_VgetRGB NULL
+#define XBIOS_VgetSize NULL
+#define XBIOS_VsetMask NULL
+#define XBIOS_VsetMode NULL
+#define XBIOS_VsetRGB NULL
+#define XBIOS_VsetScreen NULL
+#define XBIOS_VsetSync NULL
+#define XBIOS_Vsync NULL
+#define XBIOS_WavePlay NULL
+#define XBIOS_Xbtimer NULL
 
 /* XBIOS function table according to
  * http://www.yardley.cc/atari/compendium/atari-compendium-XBIOS-Function-Reference.htm
@@ -43,12 +156,118 @@ struct XBIOS_function {
     uint16_t id;
 };
 
-/* TODO fill this out */
 struct XBIOS_function XBIOS_functions[] = {
-    {"Getrez",      XBIOS_Getrez, 0x04}
+    {"Bconmap", XBIOS_Bconmap, 0x2C},
+    {"Bioskeys", XBIOS_Bioskeys, 0x18},
+    {"Blitmode", XBIOS_Blitmode, 0x40},
+    {"Buffoper", XBIOS_Buffoper, 0x88},
+    {"Buffptr", XBIOS_Buffptr, 0x8D},
+    {"Cursconf", XBIOS_Cursconf, 0x15},
+    {"Dbmsg", XBIOS_Dbmsg, 0x0B},
+    {"Devconnect", XBIOS_Devconnect, 0x8B},
+    {"DMAread", XBIOS_DMAread, 0x2A},
+    {"DMAwrite", XBIOS_DMAwrite, 0x2B},
+    {"Dosound", XBIOS_Dosound, 0x20},
+    {"Dsp_Available", XBIOS_Dsp_Available, 0x6A},
+    {"Dsp_BlkBytes", XBIOS_Dsp_BlkBytes, 0x7C},
+    {"Dsp_BlkHandShake", XBIOS_Dsp_BlkHandShake, 0x61},
+    {"Dsp_BlkUnpacked", XBIOS_Dsp_BlkUnpacked, 0x62},
+    {"Dsp_BlkWords", XBIOS_Dsp_BlkWords, 0x7B},
+    {"Dsp_DoGlock", XBIOS_Dsp_DoGlock, 0x60},
+    {"Dsp_ExecBoot", XBIOS_Dsp_ExecBoot, 0x6E},
+    {"Dsp_ExecProg", XBIOS_Dsp_ExecProg, 0x6D},
+    {"Dsp_FlushSubroutines", XBIOS_Dsp_FlushSubroutines, 0x73},
+    {"Dsp_GetProgAbility", XBIOS_Dsp_GetProgAbility, 0x72},
+    {"Dsp_GetWordSize", XBIOS_Dsp_GetWordSize, 0x67},
+    {"Dsp_Hf0", XBIOS_Dsp_Hf0, 0x77},
+    {"Dsp_Hf1", XBIOS_Dsp_Hf1, 0x78},
+    {"Dsp_Hf2", XBIOS_Dsp_Hf2, 0x79},
+    {"Dsp_Hf3", XBIOS_Dsp_Hf3, 0x7A},
+    {"Dsp_HStat", XBIOS_Dsp_HStat, 0x7D},
+    {"Dsp_InqSubrAbility", XBIOS_Dsp_InqSubrAbility, 0x75},
+    {"Dsp_InStream", XBIOS_Dsp_InStream, 0x63},
+    {"Dsp_IOStream", XBIOS_Dsp_IOStream, 0x65},
+    {"Dsp_LoadProg", XBIOS_Dsp_LoadProg, 0x6C},
+    {"Dsp_LoadSubroutine", XBIOS_Dsp_LoadSubroutine, 0x74},
+    {"Dsp_Lock", XBIOS_Dsp_Lock, 0x68},
+    {"Dsp_LodToBunary", XBIOS_Dsp_LodToBunary, 0x6F},
+    {"Dsp_MultBlocks", XBIOS_Dsp_MultBlocks, 0x7F},
+    {"Dsp_OutStream", XBIOS_Dsp_OutStream, 0x64},
+    {"Dsp_RemoveInterrupts", XBIOS_Dsp_RemoveInterrupts, 0x66},
+    {"Dsp_RequestUniqueAbility", XBIOS_Dsp_RequestUniqueAbility, 0x71},
+    {"Dsp_Reserve", XBIOS_Dsp_Reserve, 0x6B},
+    {"Dsp_RunSubroutine", XBIOS_Dsp_RunSubroutine, 0x76},
+    {"Dsp_SetVectors", XBIOS_Dsp_SetVectors, 0x7E},
+    {"Dsp_TriggerHC", XBIOS_Dsp_TriggerHC, 0x70},
+    {"Dsp_Unlock", XBIOS_Dsp_Unlock, 0x69},
+    {"Dsptristate", XBIOS_Dsptristate, 0x89},
+    {"EgetPalette", XBIOS_EgetPalette, 0x55},
+    {"EgetShift", XBIOS_EgetShift, 0x51},
+    {"EsetBank", XBIOS_EsetBank, 0x52},
+    {"EsetColor", XBIOS_EsetColor, 0x53},
+    {"EsetGray", XBIOS_EsetGray, 0x56},
+    {"EsetPalette", XBIOS_EsetPalette, 0x54},
+    {"EsetShift", XBIOS_EsetShift, 0x50},
+    {"EsetSmear", XBIOS_EsetSmear, 0x57},
+    {"Flopfmt", XBIOS_Flopfmt, 0x0A},
+    {"Floprate", XBIOS_Floprate, 0x29},
+    {"Floprd", XBIOS_Floprd, 0x08},
+    {"Flopver", XBIOS_Flopver, 0x13},
+    {"Flopwr", XBIOS_Flopwr, 0x09},
+    {"Getrez",      XBIOS_Getrez, 0x04},
+    {"Gettime", XBIOS_Gettime, 0x17},
+    {"Glaccess", XBIOS_Glaccess, 0x1C},
+    {"Gpio", XBIOS_Gpio, 0x8A},
+    {"Ikbdws", XBIOS_Ikbdws, 0x19},
+    {"Initmous", XBIOS_Initmous, 0x00},
+    {"Iorec", XBIOS_Iorec, 0x0E},
+    {"Jdisint", XBIOS_Jdisint, 0x1A},
+    {"Jenabint", XBIOS_Jenabint, 0x1B},
+    {"Kbdvbase", XBIOS_Kbdvbase, 0x22},
+    {"Kbrate", XBIOS_Kbrate, 0x23},
+    {"Keytbl", XBIOS_Keytbl, 0x10},
+    {"Locksnd", XBIOS_Locksnd, 0x80},
+    {"Logbase", XBIOS_Logbase, 0x03},
+    {"Metainit", XBIOS_Metainit, 0x30},
+    {"Mfpint", XBIOS_Mfpint, 0x0D},
+    {"Midiws", XBIOS_Midiws, 0x0C},
+    {"NVMaccess", XBIOS_NVMaccess, 0x2E},
+    {"Offgibit", XBIOS_Offgibit, 0x1D},
+    {"Ongibit", XBIOS_Ongibit, 0x1E},
+    {"Physbse", XBIOS_Physbse, 0x02},
+    {"Protobt", XBIOS_Protobt, 0x12},
+    {"Prtblk", XBIOS_Prtblk, 0x24},
+    {"Puntaes", XBIOS_Puntaes, 0x27},
+    {"Random", XBIOS_Random, 0x11},
+    {"Rsconf", XBIOS_Rsconf, 0x0F},
+    {"Scrdmp", XBIOS_Scrdmp, 0x14},
+    {"Setbuffer", XBIOS_Setbuffer, 0x83},
+    {"Setcolor", XBIOS_Setcolor, 0x07},
+    {"Setinterrupt", XBIOS_Setinterrupt, 0x87},
+    {"Setmode", XBIOS_Setmode, 0x84},
+    {"Setmontracks", XBIOS_Setmontracks, 0x86},
+    {"Setpalette", XBIOS_Setpalette, 0x06},
+    {"Setprt", XBIOS_Setprt, 0x21},
+    {"Setscreen", XBIOS_Setscreen, 0x05},
+    {"Settime", XBIOS_Settime, 0x16},
+    {"Settracks", XBIOS_Settracks, 0x85},
+    {"Sndstatus", XBIOS_Sndstatus, 0x8C},
+    {"Soundcmd", XBIOS_Soundcmd, 0x82},
+    {"Ssbrk", XBIOS_Ssbrk, 0x01},
+    {"Supexec", XBIOS_Supexec, 0x26},
+    {"Unlocksnd", XBIOS_Unlocksnd, 0x81},
+    {"VgetMonitor", XBIOS_VgetMonitor, 0x59},
+    {"VgetRGB", XBIOS_VgetRGB, 0x5E},
+    {"VgetSize", XBIOS_VgetSize, 0x5B},
+    {"VsetMask", XBIOS_VsetMask, 0x92},
+    {"VsetMode", XBIOS_VsetMode, 0x58},
+    {"VsetRGB", XBIOS_VsetRGB, 0x5D},
+    {"VsetScreen", XBIOS_VsetScreen, 0x05},
+    {"VsetSync", XBIOS_VsetSync, 0x5A},
+    {"Vsync", XBIOS_Vsync, 0x25},
+    {"WavePlay", XBIOS_WavePlay, 0xA5},
+    {"Xbtimer", XBIOS_Xbtimer, 0x1F}
 };
-
-#define XBIOS_Getrez (0x04)
 
 void xbios_trap()
 {
